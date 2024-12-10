0b42790439fa382394f87194c2684e571281814d
unknown <pierre.posson@estaca.eu>
2024-12-10 19:26:48 +0100

    Ajout thème?

---
+++ conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'projet_PK'
+copyright = '2024, Pierre Posson et Adrien Kiss'
+author = 'Pierre Posson et Adrien Kiss'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',  # Pour générer la documentation automatiquement à partir du code
+    'sphinx.ext.viewcode'  # Pour afficher le code source des fonctions et classes dans la documentation
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
